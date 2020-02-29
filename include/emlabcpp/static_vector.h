87e053c907b5a0f52d34992ee5ba9c2190854530
Jan Koniarik <jan.koniarik@trustica.cz>
2020-02-29 20:09:52 +0100

    wrote static_vector

---
+++ include/emlabcpp/static_vector.h
@@ -0,0 +1,136 @@
+#include <new>
+#include <type_traits>
+#include <utility>
+
+#pragma once
+
+namespace emlabcpp {
+
+template <typename T, std::size_t N>
+class static_vector {
+
+        using storage_type = std::aligned_storage_t<sizeof(T), alignof(T)>;
+
+      public:
+        using value_type      = T;
+        using size_type       = std::size_t;
+        using reference       = T &;
+        using const_reference = const T &;
+        using iterator        = T *;
+        using const_iterator  = const T *;
+
+        static_vector() = default;
+        static_vector(const static_vector &other) {
+                for (size_type i = 0; i < other.size(); ++i) {
+                        push_back(other[i]);
+                }
+        }
+        static_vector(static_vector &&other) noexcept {
+                for (size_type i = 0; i < other.size(); ++i) {
+                        push_back(std::move(other[i]));
+                }
+        }
+        static_vector &operator=(const static_vector &other) {
+                if (this != &other) {
+                        this->~static_vector();
+                        ::new (this) static_vector(other);
+                }
+                return *this;
+        }
+        static_vector &operator=(static_vector &&other) noexcept {
+                if (this != &other) {
+                        this->~static_vector();
+                        ::new (this) static_vector(std::move(other));
+                }
+                return *this;
+        }
+
+        [[nodiscard]] reference       front() { return ref_item(0); }
+        [[nodiscard]] const_reference front() const { return ref_item(0); }
+
+        void push_back(T item) { emplace_back(std::move(item)); }
+
+        template <typename... Args>
+        void emplace_back(Args &&... args) {
+                emplace_item(size_, std::forward<Args>(args)...);
+                size_ += 1;
+        }
+
+        T pop_back() {
+                T item = std::move(back());
+                delete_item(size_);
+                size_ -= 1;
+                return item;
+        }
+
+        [[nodiscard]] reference       back() { return ref_item(size_ - 1); }
+        [[nodiscard]] const_reference back() const { return ref_item(size_ - 1); }
+
+        [[nodiscard]] constexpr std::size_t max_size() const { return N; }
+
+        [[nodiscard]] std::size_t size() const { return size_; }
+
+        [[nodiscard]] bool empty() const { return size_ == 0; }
+
+        [[nodiscard]] bool full() const { return size_ == N; }
+
+        const_reference operator[](size_type i) const { return ref_item(i); }
+        reference       operator[](size_type i) { return ref_item(i); }
+
+        void clear() { purge(); }
+
+        ~static_vector() { purge(); }
+
+      private:
+        // private attributes
+        // --------------------------------------------------------------------------------
+
+        storage_type data_[N] = {0}; // storage of the entire dataset
+        size_type    size_    = 0;   // count of items
+
+        void delete_item(size_type i) { ref_item(i).~T(); }
+
+        template <typename... Args>
+        void emplace_item(size_type i, Args &&... args) {
+                void *gen_ptr = reinterpret_cast<void *>(&data_[i]);
+                ::new (gen_ptr) T(std::forward<Args>(args)...);
+        }
+
+        // Reference to the item in data_storage. std::launder is necessary here per the paper
+        // linked above.
+        [[nodiscard]] reference ref_item(size_type i) {
+                return *std::launder(reinterpret_cast<T *>(&data_[i]));
+        }
+        [[nodiscard]] const_reference ref_item(size_type i) const {
+                return *std::launder(reinterpret_cast<const T *>(&data_[i]));
+        }
+
+        // Cleans entire buffer from items.
+        void purge() {
+                while (!empty()) {
+                        pop_back();
+                }
+        }
+};
+
+template <typename T, std::size_t N>
+[[nodiscard]] inline bool operator==(const static_vector<T, N> &lh, const static_vector<T, N> &rh) {
+        auto size = lh.size();
+        if (size != rh.size()) {
+                return false;
+        }
+
+        for (std::size_t i = 0; i < size; ++i) {
+                if (lh[i] != rh[i]) {
+                        return false;
+                }
+        }
+        return true;
+}
+
+template <typename T, std::size_t N>
+[[nodiscard]] inline bool operator!=(const static_vector<T, N> &lh, const static_vector<T, N> &rh) {
+        return !(lh == rh);
+}
+
+} // namespace emlabcpp
---
+++ tests/static_vector_test.cpp
@@ -0,0 +1,151 @@
+#include "emlabcpp/static_vector.h"
+#include <gtest/gtest.h>
+
+using namespace emlabcpp;
+
+static constexpr std::size_t buffer_size = 10;
+
+using trivial_buffer = static_vector<int, buffer_size>;
+using obj_buffer     = static_vector<std::string, buffer_size>;
+
+TEST(static_vector_test, pop_back) {
+        trivial_buffer tbuff;
+        obj_buffer     obuff;
+
+        tbuff.push_back(33);
+        tbuff.push_back(42);
+
+        EXPECT_EQ(tbuff.back(), 42);
+        tbuff.pop_back();
+        EXPECT_EQ(tbuff.back(), 33);
+
+        obuff.push_back("Nope.");
+        obuff.push_back("Yes");
+
+        EXPECT_EQ(obuff.back(), "Yes");
+        obuff.pop_back();
+        EXPECT_EQ(obuff.back(), "Nope.");
+}
+
+TEST(static_vector_test, push_back) {
+        trivial_buffer tbuff;
+        obj_buffer     obuff;
+
+        // insert into empty buffers
+
+        tbuff.push_back(42);
+        EXPECT_EQ(tbuff.back(), 42);
+
+        obuff.push_back("The five boxing wizards jump quickly.");
+        EXPECT_EQ(obuff.back(), "The five boxing wizards jump quickly.");
+
+        // inser into non-empty buffers
+
+        tbuff.push_back(-1);
+        EXPECT_EQ(tbuff.back(), -1);
+
+        obuff.push_back("Nope.");
+        EXPECT_EQ(obuff.back(), "Nope.");
+}
+
+TEST(static_vector_test, emplace_back) {
+        trivial_buffer tbuff;
+        obj_buffer     obuff;
+
+        tbuff.emplace_back(42);
+        EXPECT_EQ(tbuff.back(), 42);
+
+        // This is special constructor of std::string
+        std::size_t i = 5;
+        obuff.emplace_back(i, 'c');
+        EXPECT_EQ(obuff.back(), "ccccc");
+}
+
+
+TEST(static_vector_test, usage) {
+        trivial_buffer tbuff;
+
+        EXPECT_TRUE(tbuff.empty());
+        EXPECT_FALSE(tbuff.full());
+        EXPECT_EQ(tbuff.size(), 0);
+        EXPECT_EQ(tbuff.max_size(), buffer_size);
+
+        tbuff.push_back(42);
+
+        EXPECT_FALSE(tbuff.empty());
+        EXPECT_FALSE(tbuff.full());
+        EXPECT_EQ(tbuff.size(), 1);
+}
+
+TEST(static_vector_test, copy_trivial) {
+        trivial_buffer tbuff;
+        for (int i : {1, 2, 3, 4, 5}) {
+                tbuff.push_back(i);
+        }
+
+        trivial_buffer cpy{tbuff};
+
+        EXPECT_EQ(tbuff, cpy);
+
+        tbuff.pop_back();
+        cpy.pop_back();
+
+        EXPECT_EQ(tbuff, cpy);
+
+        tbuff.push_back(42);
+
+        EXPECT_NE(tbuff, cpy);
+
+        trivial_buffer cpy2;
+        cpy2 = tbuff;
+
+        EXPECT_EQ(cpy2, tbuff);
+}
+
+TEST(static_vector_test, copy_object) {
+        obj_buffer obuff;
+        for (std::string s : {"Pack", "my", "box", "with", "five", "dozen", "liquor", "jugs."}) {
+                obuff.push_back(s);
+        }
+
+        obj_buffer cpy{obuff};
+        EXPECT_EQ(obuff, cpy);
+
+        obj_buffer cpy2;
+        cpy2 = obuff;
+        EXPECT_EQ(obuff, cpy2);
+}
+
+TEST(static_vector_test, move_trivial) {
+        trivial_buffer tbuff;
+        for (int i : {1, 2, 3, 4, 5}) {
+                tbuff.push_back(i);
+        }
+
+        trivial_buffer cpy{tbuff};
+        trivial_buffer moved{std::move(tbuff)};
+
+        EXPECT_EQ(cpy, moved);
+
+        trivial_buffer moved2;
+        moved2 = std::move(moved);
+
+        EXPECT_EQ(cpy, moved2);
+}
+
+TEST(static_vector_test, move_object) {
+        obj_buffer obuff;
+        for (std::string s : {"Pack", "my", "box", "with", "five", "dozen", "liquor", "jugs."}) {
+                obuff.push_back(s);
+        }
+
+        obj_buffer cpy{obuff};
+        obj_buffer moved{std::move(obuff)};
+
+        EXPECT_EQ(cpy, moved);
+
+        obj_buffer moved2;
+        moved2 = std::move(moved);
+
+        EXPECT_EQ(cpy, moved2);
+}
