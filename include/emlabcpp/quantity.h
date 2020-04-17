191734c93517aad5086d5fb337d2ff2b81a028f0
Jan Koniarik <jan.koniarik@trustica.cz>
2020-04-17 12:59:17 +0200

    renamed template parameter for quantity

--- include/emlabcpp/quantity.h
+++ include/emlabcpp/quantity.h
@@ -27,12 +27,12 @@ namespace emlabcpp {
 /// Credits should go to https://github.com/joboccara/NamedType as I inspired by project by this
 /// blogger!
 ///
-template <typename T, typename ValueType = float>
+template <typename Derived, typename ValueType = float>
 class quantity {
         ValueType value_;
 
-        [[nodiscard]] T &      impl() { return static_cast<T &>(*this); }
-        [[nodiscard]] T const &impl() const { return static_cast<T const &>(*this); }
+        [[nodiscard]] Derived &      impl() { return static_cast<Derived &>(*this); }
+        [[nodiscard]] Derived const &impl() const { return static_cast<Derived const &>(*this); }
 
       public:
         using value_type = ValueType;
@@ -45,26 +45,26 @@ class quantity {
         /// Const reference to the internal value
         constexpr ValueType operator*() const noexcept { return value_; }
 
-        /// Add other quantity of same T and value_type
-        constexpr T &operator+=(const quantity other) noexcept {
+        /// Add other quantity of same Derived and value_type
+        constexpr Derived &operator+=(const quantity other) noexcept {
                 value_ += *other;
                 return impl();
         }
 
-        /// Subtract other quantity of same T and value_type
-        constexpr T &operator-=(const quantity other) noexcept {
+        /// Subtract other quantity of same Derived and value_type
+        constexpr Derived &operator-=(const quantity other) noexcept {
                 value_ -= *other;
                 return impl();
         }
 
         /// Divides quantity by it's value type
-        constexpr T &operator/=(const ValueType val) noexcept {
+        constexpr Derived &operator/=(const ValueType val) noexcept {
                 value_ /= val;
                 return impl();
         }
 
         /// Multiplies quantity by it's value type
-        constexpr T &operator*=(const ValueType val) noexcept {
+        constexpr Derived &operator*=(const ValueType val) noexcept {
                 value_ *= val;
                 return impl();
         }
@@ -74,113 +74,125 @@ class quantity {
         constexpr explicit operator U() const noexcept {
                 return U(value_);
         }
+
+        static std::string get_unit() { return ""; }
 };
 
-/// Sum of quantities with same T and value_type
-template <typename T, typename ValueType>
-constexpr T operator+(quantity<T, ValueType> lhs, const quantity<T, ValueType> rhs) {
+/// Sum of quantities with same Derived and value_type
+template <typename Derived, typename ValueType>
+constexpr Derived operator+(quantity<Derived, ValueType>       lhs,
+                            const quantity<Derived, ValueType> rhs) {
         return lhs += rhs;
 }
 
-/// Subtraction of quantities with same T and value_type
-template <typename T, typename ValueType>
-constexpr T operator-(quantity<T, ValueType> lhs, const quantity<T, ValueType> rhs) {
+/// Subtraction of quantities with same Derived and value_type
+template <typename Derived, typename ValueType>
+constexpr Derived operator-(quantity<Derived, ValueType>       lhs,
+                            const quantity<Derived, ValueType> rhs) {
         return lhs -= rhs;
 }
 
 /// Provides negation of the quantity
-template <typename T, typename ValueType>
-constexpr T operator-(const quantity<T, ValueType> val) {
-        return T{-*val};
+template <typename Derived, typename ValueType>
+constexpr Derived operator-(const quantity<Derived, ValueType> val) {
+        return Derived{-*val};
 }
 
 /// Comparison of internal values of quantity
-template <typename T, typename ValueType>
-constexpr bool operator==(const quantity<T, ValueType> lhs, const quantity<T, ValueType> rhs) {
+template <typename Derived, typename ValueType>
+constexpr bool operator==(const quantity<Derived, ValueType> lhs,
+                          const quantity<Derived, ValueType> rhs) {
         return *lhs == *rhs;
 }
 
 /// Comparasion of internal values
-template <typename T, typename ValueType>
-constexpr bool operator<(const quantity<T, ValueType> lhs, const quantity<T, ValueType> rhs) {
+template <typename Derived, typename ValueType>
+constexpr bool operator<(const quantity<Derived, ValueType> lhs,
+                         const quantity<Derived, ValueType> rhs) {
         return *lhs < *rhs;
 }
 
 /// Multiplication of quantity by it's value_type
-template <typename T, typename ValueType>
-constexpr T operator*(quantity<T, ValueType> q, const ValueType val) {
+template <typename Derived, typename ValueType>
+constexpr Derived operator*(quantity<Derived, ValueType> q, const ValueType val) {
         return q *= val;
 }
 
 /// Division of quantity by it's value_type
-template <typename T, typename ValueType>
-constexpr T operator/(quantity<T, ValueType> q, const ValueType val) {
+template <typename Derived, typename ValueType>
+constexpr Derived operator/(quantity<Derived, ValueType> q, const ValueType val) {
         return q /= val;
 }
 
 /// Quantity with absolute value of internal value
-template <typename T, typename ValueType>
-constexpr T abs(const quantity<T, ValueType> q) {
-        return T(std::abs(*q));
+template <typename Derived, typename ValueType>
+constexpr Derived abs(const quantity<Derived, ValueType> q) {
+        return Derived(std::abs(*q));
 }
 
 /// Returns cosinus of the quantity as scalar
-template <typename T, typename ValueType>
-constexpr auto cos(const quantity<T, ValueType> u) {
+template <typename Derived, typename ValueType>
+constexpr auto cos(const quantity<Derived, ValueType> u) {
         return std::cos(*u);
 }
 
 /// Returns sinus of the quantity as scalar
-template <typename T, typename ValueType>
-constexpr auto sin(const quantity<T, ValueType> u) {
+template <typename Derived, typename ValueType>
+constexpr auto sin(const quantity<Derived, ValueType> u) {
         return std::sin(*u);
 }
 
 /// Quantity with maximum value of one of the quantities
-template <typename T, typename ValueType>
-constexpr T max(const quantity<T, ValueType> lh, const quantity<T, ValueType> rh) {
+template <typename Derived, typename ValueType>
+constexpr Derived max(const quantity<Derived, ValueType> lh,
+                      const quantity<Derived, ValueType> rh) {
         return T(std::max(*lh, *rh));
 }
 
 /// Quantity with minimum value of one of the quantities
-template <typename T, typename ValueType>
-constexpr T min(const quantity<T, ValueType> lh, const quantity<T, ValueType> rh) {
+template <typename Derived, typename ValueType>
+constexpr Derived min(const quantity<Derived, ValueType> lh,
+                      const quantity<Derived, ValueType> rh) {
         return T(std::min(*lh, *rh));
 }
 
 //---------------------------------------------------------------------------
 
 /// Non-equality of quantites is negation of equality.
-template <typename T, typename ValueType>
-constexpr bool operator!=(const quantity<T, ValueType> lhs, const quantity<T, ValueType> rhs) {
+template <typename Derived, typename ValueType>
+constexpr bool operator!=(const quantity<Derived, ValueType> lhs,
+                          const quantity<Derived, ValueType> rhs) {
         return !(lhs == rhs);
 }
 
 /// Q1 > Q2 iff Q2 < Q1
-template <typename T, typename ValueType>
-constexpr bool operator>(const quantity<T, ValueType> lhs, const quantity<T, ValueType> rhs) {
+template <typename Derived, typename ValueType>
+constexpr bool operator>(const quantity<Derived, ValueType> lhs,
+                         const quantity<Derived, ValueType> rhs) {
         return rhs < lhs;
 }
 /// Q1 <= Q2 iff !( Q2 > Q1 )
-template <typename T, typename ValueType>
-constexpr bool operator<=(const quantity<T, ValueType> lhs, const quantity<T, ValueType> rhs) {
+template <typename Derived, typename ValueType>
+constexpr bool operator<=(const quantity<Derived, ValueType> lhs,
+                          const quantity<Derived, ValueType> rhs) {
         return !(lhs > rhs);
 }
 /// Q1 >= Q2 iff !( Q2 < Q1 )
-template <typename T, typename ValueType>
-constexpr bool operator>=(const quantity<T, ValueType> lhs, const quantity<T, ValueType> rhs) {
+template <typename Derived, typename ValueType>
+constexpr bool operator>=(const quantity<Derived, ValueType> lhs,
+                          const quantity<Derived, ValueType> rhs) {
         return !(lhs < rhs);
 }
 //---------------------------------------------------------------------------
 
 /// Multiplication of value_type by quantity returns quantity
-template <typename T, typename ValueType>
-constexpr T operator*(const ValueType val, const quantity<T, ValueType> q) {
+template <typename Derived, typename ValueType>
+constexpr Derived operator*(const ValueType val, const quantity<Derived, ValueType> q) {
         return q * val;
 }
 /// Division of value_type by quantity returns quantity
-template <typename T, typename ValueType>
-constexpr ValueType operator/(const ValueType val, const quantity<T, ValueType> q) {
+template <typename Derived, typename ValueType>
+constexpr ValueType operator/(const ValueType val, const quantity<Derived, ValueType> q) {
         return val / *q;
 }
 
@@ -188,20 +200,22 @@ constexpr ValueType operator/(const ValueType val, const quantity<T, ValueType> 
 
 /// The quantity has defined partital specialization of std::numeric_limits,
 /// works as std::numeric_limits<ValueType>;
-template <typename T, typename ValueType>
-struct std::numeric_limits<emlabcpp::quantity<T, ValueType>> {
-        constexpr static T lowest() { return T{std::numeric_limits<ValueType>::lowest()}; }
-        constexpr static T min() { return T{std::numeric_limits<ValueType>::min()}; }
-        constexpr static T max() { return T{std::numeric_limits<ValueType>::max()}; }
+template <typename Derived, typename ValueType>
+struct std::numeric_limits<emlabcpp::quantity<Derived, ValueType>> {
+        constexpr static Derived lowest() {
+                return Derived{std::numeric_limits<ValueType>::lowest()};
+        }
+        constexpr static Derived min() { return Derived{std::numeric_limits<ValueType>::min()}; }
+        constexpr static Derived max() { return Derived{std::numeric_limits<ValueType>::max()}; }
 };
 
-/// Hash of quantity is hash of it's value and T::get_unit() xored.
-template <typename T, typename ValueType>
-struct std::hash<emlabcpp::quantity<T, ValueType>> {
-        std::size_t operator()(const emlabcpp::quantity<T, ValueType> q) {
+/// Hash of quantity is hash of it's value and Derived::get_unit() xored.
+template <typename Derived, typename ValueType>
+struct std::hash<emlabcpp::quantity<Derived, ValueType>> {
+        std::size_t operator()(const emlabcpp::quantity<Derived, ValueType> q) {
                 // TODO: this should be rewritten
                 // 'reverse' the prefix+unit info in bits and than xor it with number
-                std::string unit = T::get_unit();
+                std::string unit = Derived::get_unit();
                 return std::hash<ValueType>()(*q) ^ std::hash<std::string>()(unit);
         }
 };
