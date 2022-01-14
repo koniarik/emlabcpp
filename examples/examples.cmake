5beb9f4487f3dec5ac95e52cf0c983e632983215
Jan Koniarik <veverak@mail.muni.cz>
2022-01-14 16:41:44 +0100

    changed the CMakeLists.txt a bit

--- examples/examples.cmake
+++
@@ -1,9 +0,0 @@
-
-function(add_emlabcpp_example name)
-	add_executable(${name}_example examples/${name}.cpp)
-	emlabcpp_setup_test(${name}_example)
-endfunction()
-
-add_emlabcpp_example(algorithm)
-add_emlabcpp_example(protocol)
-
