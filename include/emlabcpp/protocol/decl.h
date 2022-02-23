0e7d916e6181b387023cdeb7c0b4fc1dbc0e2b39
Jan Veverak Koniarik <squirrelcze@gmail.com>
2022-02-23 17:13:28 +0100

    added support for better json definition output

--- include/emlabcpp/protocol/decl.h
+++ include/emlabcpp/protocol/decl.h
@@ -20,12 +20,15 @@ struct protocol_decl;
 // This concepts limits types to types that can be declared, that is the overload of 'protocol_decl'
 // is fully defined: protocol_decl::value_type contains definition of value produced by the
 // declaration, protocol_decl::max_size contains estimated maximal size in bytes taken by the
-// serialized value in the message.
+// serialized value in the message. protocol_decl::min_size should contain minimal size used.
 template < typename D >
 concept protocol_declarable = requires( D val )
 {
         {
                 protocol_decl< D >::max_size
+                } -> std::convertible_to< std::size_t >;
+        {
+                protocol_decl< D >::min_size
                 } -> std::convertible_to< std::size_t >;
         std::default_initializable< typename protocol_decl< D >::value_type >;
 };
@@ -35,6 +38,7 @@ struct protocol_decl< D >
 {
         using value_type                      = D;
         static constexpr std::size_t max_size = sizeof( D );
+        static constexpr std::size_t min_size = max_size;
 };
 
 template < protocol_declarable D, std::size_t N >
@@ -42,6 +46,7 @@ struct protocol_decl< std::array< D, N > >
 {
         using value_type                      = std::array< D, N >;
         static constexpr std::size_t max_size = protocol_decl< D >::max_size * N;
+        static constexpr std::size_t min_size = protocol_decl< D >::min_size * N;
 };
 
 template < protocol_declarable... Ds >
@@ -49,6 +54,7 @@ struct protocol_decl< std::tuple< Ds... > >
 {
         using value_type = std::tuple< typename protocol_decl< Ds >::value_type... >;
         static constexpr std::size_t max_size = ( protocol_decl< Ds >::max_size + ... + 0 );
+        static constexpr std::size_t min_size = ( protocol_decl< Ds >::min_size + ... + 0 );
 };
 
 template < protocol_declarable... Ds >
@@ -60,6 +66,8 @@ struct protocol_decl< std::variant< Ds... > >
 
         static constexpr std::size_t max_size =
             id_decl::max_size + std::max< std::size_t >( { protocol_decl< Ds >::max_size... } );
+        static constexpr std::size_t min_size =
+            id_decl::min_size + std::min< std::size_t >( { protocol_decl< Ds >::min_size... } );
 };
 
 template < std::size_t N >
@@ -68,6 +76,7 @@ struct protocol_decl< std::bitset< N > >
         using value_type = std::bitset< N >;
 
         static constexpr std::size_t max_size = ( N + 7 ) / 8;
+        static constexpr std::size_t min_size = max_size;
 };
 
 template < std::size_t N >
@@ -76,6 +85,7 @@ struct protocol_decl< protocol_sizeless_message< N > >
         using value_type = protocol_sizeless_message< N >;
 
         static constexpr std::size_t max_size = N;
+        static constexpr std::size_t min_size = max_size;
 };
 
 template < protocol_declarable D, auto Offset >
@@ -86,6 +96,7 @@ struct protocol_decl< protocol_offset< D, Offset > >
         using value_type = typename def_decl::value_type;
 
         static constexpr std::size_t max_size = def_decl::max_size;
+        static constexpr std::size_t min_size = def_decl::min_size;
 };
 
 template < quantity_derived D >
@@ -94,6 +105,7 @@ struct protocol_decl< D >
         using value_type = D;
 
         static constexpr std::size_t max_size = protocol_decl< typename D::value_type >::max_size;
+        static constexpr std::size_t min_size = protocol_decl< typename D::value_type >::min_size;
 };
 
 template < protocol_declarable D, D Min, D Max >
@@ -102,6 +114,7 @@ struct protocol_decl< bounded< D, Min, Max > >
         using value_type = bounded< D, Min, Max >;
 
         static constexpr std::size_t max_size = protocol_decl< D >::max_size;
+        static constexpr std::size_t min_size = protocol_decl< D >::min_size;
 };
 
 template < protocol_declarable CounterType, protocol_declarable D >
@@ -112,6 +125,7 @@ struct protocol_decl< protocol_sized_buffer< CounterType, D > >
         using value_type   = typename sub_decl::value_type;
 
         static constexpr std::size_t max_size = counter_decl::max_size + sub_decl::max_size;
+        static constexpr std::size_t min_size = counter_decl::min_size + sub_decl::min_size;
 };
 
 template < auto V >
@@ -121,6 +135,7 @@ struct protocol_decl< tag< V > >
         using value_type = tag< V >;
 
         static constexpr std::size_t max_size = sub_decl::max_size;
+        static constexpr std::size_t min_size = sub_decl::min_size;
 };
 
 template < protocol_declarable... Ds >
@@ -130,6 +145,9 @@ struct protocol_decl< protocol_group< Ds... > >
 
         static constexpr std::size_t max_size =
             std::max< std::size_t >( { protocol_decl< Ds >::max_size..., 0 } );
+        static constexpr std::size_t min_size = std::min< std::size_t >(
+            { protocol_decl< Ds >::min_size...,
+              sizeof...( Ds ) == 0 ? 0 : std::numeric_limits< std::size_t >::max() } );
 };
 
 template < protocol_endianess_enum Endianess, protocol_declarable D >
@@ -147,6 +165,7 @@ struct protocol_decl< protocol_mark >
 {
         using value_type                      = protocol_mark;
         static constexpr std::size_t max_size = protocol_mark{}.max_size();
+        static constexpr std::size_t min_size = max_size;
 };
 
 template <>
@@ -154,10 +173,14 @@ struct protocol_decl< protocol_error_record >
 {
         using value_type = protocol_error_record;
 
-        using mark_decl   = protocol_decl< protocol_mark >;
-        using offset_decl = protocol_decl< std::size_t >;
+        using mark_type   = protocol_mark;
+        using offset_type = std::size_t;
+
+        using mark_decl   = protocol_decl< mark_type >;
+        using offset_decl = protocol_decl< offset_type >;
 
         static constexpr std::size_t max_size = mark_decl::max_size + offset_decl::max_size;
+        static constexpr std::size_t min_size = mark_decl::min_size + offset_decl::min_size;
 };
 
 template < typename T, std::size_t N >
@@ -167,6 +190,8 @@ struct protocol_decl< static_vector< T, N > >
         using counter_type = uint16_t;
         static constexpr std::size_t max_size =
             protocol_decl< counter_type >::max_size + protocol_decl< T >::max_size * N;
+        static constexpr std::size_t min_size =
+            protocol_decl< counter_type >::min_size + protocol_decl< T >::min_size * N;
 };
 
 }  // namespace emlabcpp
--- include/emlabcpp/protocol/json.h
+++ include/emlabcpp/protocol/json.h
@@ -6,6 +6,11 @@
 #include <magic_enum.hpp>
 #endif
 
+#ifdef EMLABCPP_USE_DEMANGLING
+#include <cxxabi.h>
+#endif
+
+#include "emlabcpp/algorithm.h"
 #include "emlabcpp/protocol/decl.h"
 
 #pragma once
@@ -35,8 +40,13 @@ struct nlohmann::adl_serializer< emlabcpp::protocol_decl< D > >
 
         static void to_json( nlohmann::json& j, const decl& )
         {
-                j["type"]     = prot_ser::name;
+                j["type"] = prot_ser::type_name;
+#ifdef EMLABCPP_USE_DEMANGLING
+                j["pretty_name"] = prot_ser::get_name();
+#endif
+                j["raw_name"] = typeid( D ).name();
                 j["max_size"] = decl::max_size;
+                j["min_size"] = decl::min_size;
                 prot_ser::add_extra( j );
         }
 
@@ -52,50 +62,87 @@ namespace emlabcpp
 template <>
 struct protocol_json_serializer< uint8_t > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "uint8_t";
+        static constexpr std::string_view type_name = "uint8_t";
+        static std::string                get_name()
+        {
+                return std::string{ type_name };
+        }
 };
 template <>
 struct protocol_json_serializer< uint16_t > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "uint16_t";
+        static constexpr std::string_view type_name = "uint16_t";
+        static std::string                get_name()
+        {
+                return std::string{ type_name };
+        }
 };
 template <>
 struct protocol_json_serializer< uint32_t > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "uint32_t";
+        static constexpr std::string_view type_name = "uint32_t";
+        static std::string                get_name()
+        {
+                return std::string{ type_name };
+        }
 };
 template <>
 struct protocol_json_serializer< uint64_t > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "uint64_t";
+        static constexpr std::string_view type_name = "uint64_t";
+        static std::string                get_name()
+        {
+                return std::string{ type_name };
+        }
 };
 template <>
 struct protocol_json_serializer< int8_t > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "int8_t";
+        static constexpr std::string_view type_name = "int8_t";
+        static std::string                get_name()
+        {
+                return std::string{ type_name };
+        }
 };
 template <>
 struct protocol_json_serializer< int16_t > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "int16_t";
+        static constexpr std::string_view type_name = "int16_t";
+        static std::string                get_name()
+        {
+                return std::string{ type_name };
+        }
 };
 template <>
 struct protocol_json_serializer< int32_t > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "int32_t";
+        static constexpr std::string_view type_name = "int32_t";
+        static std::string                get_name()
+        {
+                return std::string{ type_name };
+        }
 };
 template <>
 struct protocol_json_serializer< int64_t > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "int64_t";
+        static constexpr std::string_view type_name = "int64_t";
+        static std::string                get_name()
+        {
+                return std::string{ type_name };
+        }
 };
 template < protocol_declarable T >
 requires( std::is_enum_v< T > ) struct protocol_json_serializer< T > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "enum";
+        static constexpr std::string_view type_name = "enum";
+        static std::string                get_name()
+        {
+                return pretty_type_name< T >();
+        }
 
         static void add_extra( nlohmann::json& j )
         {
+                j["sub_type"] = protocol_decl< std::underlying_type_t< T > >{};
 #ifdef EMLABCPP_USE_MAGIC_ENUM
                 j["values"] = magic_enum::enum_names< T >();
 #else
@@ -107,7 +154,13 @@ requires( std::is_enum_v< T > ) struct protocol_json_serializer< T > : protocol_
 template < protocol_declarable D, std::size_t N >
 struct protocol_json_serializer< std::array< D, N > > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "array";
+        static constexpr std::string_view type_name = "array";
+        using sub_ser                               = protocol_json_serializer< D >;
+
+        static std::string get_name()
+        {
+                return sub_ser::get_name() + "[" + std::to_string( N ) + "]";
+        }
 
         static void add_extra( nlohmann::json& j )
         {
@@ -119,7 +172,13 @@ struct protocol_json_serializer< std::array< D, N > > : protocol_json_serializer
 template < protocol_declarable... Ds >
 struct protocol_json_serializer< std::tuple< Ds... > > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "tuple";
+        static constexpr std::string_view type_name = "tuple";
+
+        static std::string get_name()
+        {
+                std::vector< std::string > names{ protocol_json_serializer< Ds >::get_name()... };
+                return "(" + joined( names, std::string{ "," } ) + ")";
+        }
 
         static void add_extra( nlohmann::json& j )
         {
@@ -133,10 +192,20 @@ struct protocol_json_serializer< std::tuple< Ds... > > : protocol_json_serialize
 template < protocol_declarable... Ds >
 struct protocol_json_serializer< std::variant< Ds... > > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "variant";
+        static constexpr std::string_view type_name = "variant";
+
+        using decl = protocol_decl< std::variant< Ds... > >;
+
+        static std::string get_name()
+        {
+                std::vector< std::string > names{ protocol_json_serializer< Ds >::get_name()... };
+                return protocol_json_serializer< typename decl::id_type >::get_name() + ",{" +
+                       joined( names, std::string{ "|" } ) + "}";
+        }
 
         static void add_extra( nlohmann::json& j )
         {
+                j["counter_type"] = typename decl::id_decl{};
                 j["sub_types"] =
                     map_f_to_a( std::tuple< Ds... >{}, [&]< typename D >( D ) -> nlohmann::json {
                             return protocol_decl< D >{};
@@ -147,7 +216,12 @@ struct protocol_json_serializer< std::variant< Ds... > > : protocol_json_seriali
 template < std::size_t N >
 struct protocol_json_serializer< std::bitset< N > > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "bitset";
+        static constexpr std::string_view type_name = "bitset";
+
+        static std::string get_name()
+        {
+                return "bitset[" + std::to_string( N ) + "]";
+        }
 
         static void add_extra( nlohmann::json& j )
         {
@@ -158,13 +232,19 @@ struct protocol_json_serializer< std::bitset< N > > : protocol_json_serializer_b
 template < std::size_t N >
 struct protocol_json_serializer< protocol_sizeless_message< N > > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "sizeless_message";
+        static constexpr std::string_view type_name = "sizeless_message";
 };
 
 template < protocol_declarable D, auto Offset >
 struct protocol_json_serializer< protocol_offset< D, Offset > > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "offset";
+        static constexpr std::string_view type_name = "offset";
+
+        static std::string get_name()
+        {
+                return protocol_json_serializer< D >::get_string() + " + " +
+                       std::to_string( Offset );
+        }
 
         static void add_extra( nlohmann::json& j )
         {
@@ -177,7 +257,12 @@ template < quantity_derived D >
 requires( protocol_declarable< D > ) struct protocol_json_serializer< D >
   : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "quantity";
+        static constexpr std::string_view type_name = "quantity";
+
+        static std::string get_name()
+        {
+                return protocol_json_serializer< typename D::value_type >::get_name();
+        }
 
         static void add_extra( nlohmann::json& j )
         {
@@ -191,7 +276,12 @@ requires( protocol_declarable< D > ) struct protocol_json_serializer< D >
 template < protocol_declarable D, D Min, D Max >
 struct protocol_json_serializer< bounded< D, Min, Max > > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "bounded";
+        static constexpr std::string_view type_name = "bounded";
+
+        static std::string get_name()
+        {
+                return ">" + protocol_json_serializer< D >::get_name() + "<";
+        }
 
         static void add_extra( nlohmann::json& j )
         {
@@ -205,7 +295,12 @@ template < protocol_declarable CounterType, protocol_declarable D >
 struct protocol_json_serializer< protocol_sized_buffer< CounterType, D > >
   : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "sized_buffer";
+        static constexpr std::string_view type_name = "sized_buffer";
+
+        static std::string get_name()
+        {
+                return std::string{ type_name };
+        }
 
         static void add_extra( nlohmann::json& j )
         {
@@ -217,19 +312,29 @@ struct protocol_json_serializer< protocol_sized_buffer< CounterType, D > >
 template < auto V >
 struct protocol_json_serializer< tag< V > > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "tag";
+        static constexpr std::string_view type_name = "tag";
+
+        static std::string get_name()
+        {
+#ifdef EMLABCPP_USE_MAGIC_ENUM
+                if constexpr ( std::is_enum_v< decltype( V ) > ) {
+                        return "tag<" + std::string{ magic_enum::enum_name( V ) } + ">";
+                } else {
+                        return "tag<" + std::to_string( V ) + ">";
+                }
+#else
+                return "tag<" + std::to_string( V ) + ">";
+#endif
+        }
 
         static void add_extra( nlohmann::json& j )
         {
                 j["sub_type"] = typename protocol_decl< tag< V > >::sub_decl{};
+                j["value"]    = V;
 #ifdef EMLABCPP_USE_MAGIC_ENUM
                 if constexpr ( std::is_enum_v< decltype( V ) > ) {
-                        j["value"] = magic_enum::enum_name( V );
-                } else {
-                        j["value"] = V;
+                        j["enumerator"] = magic_enum::enum_name( V );
                 }
-#else
-                j["value"] = V;
 #endif
         }
 };
@@ -237,7 +342,13 @@ struct protocol_json_serializer< tag< V > > : protocol_json_serializer_base
 template < protocol_declarable... Ds >
 struct protocol_json_serializer< protocol_group< Ds... > > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "group";
+        static constexpr std::string_view type_name = "group";
+
+        static std::string get_name()
+        {
+                std::vector< std::string > names{ protocol_json_serializer< Ds >::get_name()... };
+                return "{" + joined( names, std::string{ "|" } ) + "}";
+        }
 
         static void add_extra( nlohmann::json& j )
         {
@@ -258,32 +369,52 @@ template < std::derived_from< protocol_def_type_base > D >
 requires( protocol_declarable< D > ) struct protocol_json_serializer< D >
   : protocol_json_serializer< typename D::def_type >
 {
+        static std::string get_name()
+        {
+                return pretty_type_name< D >();
+        }
 };
 
 template <>
 struct protocol_json_serializer< protocol_mark > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "mark";
+        static constexpr std::string_view type_name = "mark";
+        static std::string                get_name()
+        {
+                return std::string{ type_name };
+        }
 };
 
 template <>
 struct protocol_json_serializer< protocol_error_record > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "error_record";
+        static constexpr std::string_view type_name = "error_record";
+
+        static std::string get_name()
+        {
+                return std::string{ type_name };
+        }
 
         using decl = protocol_decl< protocol_error_record >;
 
         static void add_extra( nlohmann::json& j )
         {
-                j["mark_type"]   = typename decl::mark_decl{};
-                j["offset_type"] = typename decl::offset_decl{};
+                j["mark_type"]   = protocol_decl< typename decl::mark_type >{};
+                j["offset_type"] = protocol_decl< typename decl::offset_type >{};
         }
 };
 
 template < typename T, std::size_t N >
 struct protocol_json_serializer< static_vector< T, N > > : protocol_json_serializer_base
 {
-        static constexpr std::string_view name = "static_vector";
+        static constexpr std::string_view type_name = "static_vector";
+        using sub_ser                               = protocol_json_serializer< T >;
+
+        static std::string get_name()
+        {
+                return sub_ser::get_name() + "[" + std::to_string( N ) + "]";
+                ;
+        }
 
         static void add_extra( nlohmann::json& j )
         {
