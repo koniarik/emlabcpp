ec485a74423375dbc47ad0e568afcbc92f9c101d
Jan Veverak Koniarik <squirrelcze@gmail.com>
2022-03-02 17:10:24 +0100

    now testing protocol message conversion suffix

--- tests/protocol_base_test.cpp
+++ tests/protocol_base_test.cpp
@@ -7,10 +7,13 @@ using namespace emlabcpp;
 
 TEST( Protocol, message_conversion )
 {
-        std::array< uint8_t, 4 > data = { 53, 3, 64, 85 };
+        std::array< uint8_t, 4 > data   = { 53, 3, 64, 85 };
+        std::array< uint8_t, 4 > suffix = { 0, 0, 0, 0 };
         protocol_message< 4 >    m{ data };
 
         auto buff = std::array< uint8_t, 8 >( m );
 
         EXPECT_EQ( view_n( buff.begin(), 4 ), view{ data } );
+
+        EXPECT_EQ( view_n( buff.begin() + 4, 4 ), view{ suffix } );
 }
