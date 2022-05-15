74adc1137298cdb88b33fcc908f07f159435f12f
Jan Veverak Koniarik <squirrelcze@gmail.com>
2022-05-15 09:30:07 +0200

    Added types test and reconfigured ctest

---
+++ tests/types_test.cpp
@@ -0,0 +1,13 @@
+#include <emlabcpp/types.h>
+#include <emlabcpp/view.h>
+#include <gtest/gtest.h>
+#include <list>
+
+using namespace emlabcpp;
+
+static_assert( std::same_as< mapped_t< std::vector< int >, std::identity >, const int& > );
+static_assert( std::same_as< mapped_t< std::list< int >, std::identity >, const int& > );
+static_assert( std::same_as< mapped_t< view< const int* >, std::identity >, const int& > );
+// TODO: decide this:
+// static_assert(
+//    std::same_as< mapped_t< view< const int ( * )[128] >, std::identity >, const int& > );
