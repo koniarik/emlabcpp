f3f13abf8d35b3e11596f53d368febecefa004d3
Jan Veverak Koniarik <squirrelcze@gmail.com>
2022-08-07 17:52:42 +0200

    added enum utility to switch between normal conversion/magic enum at compile time

---
+++ include/emlabcpp/enum.h
@@ -0,0 +1,22 @@
+#ifdef EMLABCPP_USE_MAGIC_ENUM
+
+#include <magic_enum.hpp>
+
+#endif
+
+#pragma once
+
+namespace emlabcpp
+{
+
+template < typename Enum >
+auto convert_enum( Enum val )
+{
+#ifdef EMLABCPP_USE_MAGIC_ENUM
+        return magic_enum::enum_name( val );
+#else
+        return std::to_string( val );
+#endif
+}
+
+};  // namespace emlabcpp
