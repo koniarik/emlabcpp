6dc49ec99eaadec44defde75c2bf3c76eba511cb
Jan Veverak Koniarik <squirrelcze@gmail.com>
2022-08-12 19:20:07 +0200

    Refactored logging

--- include/emlabcpp/experimental/logging.h
+++ include/emlabcpp/experimental/logging.h
@@ -10,12 +10,20 @@ namespace emlabcpp
 {
 inline void log( const char* file, int line, const std::string& msg )
 {
-        // \x1B[31mTexting\033[0m
         std::filesystem::path p{ file };
+
+        std::ostream& os = std::cout;
+
+        auto colorize = [&]( std::string_view color, const auto& item ) {
+                os << "\033[38;5;" << color << "m" << item;
+                return "\033[0m";
+        };
+
         std::time_t t = std::chrono::system_clock::to_time_t( std::chrono::system_clock::now() );
-        std::cout << "\033[38;5;33m" << std::put_time( std::localtime( &t ), "%T " )
-                  << "\033[38;5;128m" << p.stem().native() << " \033[38;5;164m" << line
-                  << "\033[0m  " << msg << "\n";
+        os << colorize( "33", std::put_time( std::localtime( &t ), "%T" ) ) << " "  // time
+           << colorize( "128", p.stem().native() ) << " "                           // filename
+           << colorize( "164", line ) << " "                                        // line
+           << msg << "\n";
 }
 }  // namespace emlabcpp
 
