3ebcdc84b9e9231be1d05595351b25a794b0e0c8
Jan Veverak Koniarik <squirrelcze@gmail.com>
2022-08-16 09:00:12 +0200

    implemented history behavior for logging

--- src/experimental/logging.cpp
+++ src/experimental/logging.cpp
@@ -1,5 +1,7 @@
 #include "emlabcpp/experimental/logging.h"
 
+#include "emlabcpp/static_circular_buffer.h"
+
 #ifdef EMLABCPP_LOGGING_ENABLED
 
 #include <filesystem>
@@ -7,26 +9,61 @@
 
 namespace emlabcpp
 {
+
+namespace
+{
+        static constexpr std::size_t log_buffer_size = 128;
+        using log_tp     = std::chrono::time_point< std::chrono::system_clock >;
+        using log_tpl    = std::tuple< log_tp, const char*, int, std::string, log_severity >;
+        using log_buffer = static_circular_buffer< log_tpl, log_buffer_size >;
+
+        thread_local log_buffer LOG_BUFFER;
+
+        void log_print_msg(
+            log_tp             tp,
+            const char*        file,
+            int                line,
+            const std::string& msg,
+            log_severity       severity )
+        {
+                std::string_view tcolor = severity == log_severity::INFO ? "33" : "250";
+                std::string_view fcolor = severity == log_severity::INFO ? "128" : "252";
+                std::string_view lcolor = severity == log_severity::INFO ? "164" : "248";
+
+                std::filesystem::path p{ file };
+
+                std::ostream& os = std::cout;
+
+                auto colorize = [&]( const std::string_view& color, const auto& item ) {
+                        os << "\033[38;5;" << color << "m" << item;
+                        return "\033[0m";
+                };
+
+                std::time_t t = std::chrono::system_clock::to_time_t( tp );
+                os << colorize( tcolor, std::put_time( std::localtime( &t ), "%T" ) )
+                   << " "                                           // time
+                   << colorize( fcolor, p.stem().native() ) << " "  // filename
+                   << colorize( lcolor, line ) << " "               // line
+                   << msg << "\n";
+        }
+}  // namespace
+
 void log( const char* file, int line, const std::string& msg, log_severity severity )
 {
-        std::string_view tcolor = severity == log_severity::INFO ? "33" : "250";
-        std::string_view fcolor = severity == log_severity::INFO ? "128" : "252";
-        std::string_view lcolor = severity == log_severity::INFO ? "164" : "248";
+        log_tp tp = std::chrono::system_clock::now();
+        if ( severity == log_severity::DEBUG ) {
+                if ( LOG_BUFFER.full() ) {
+                        LOG_BUFFER.pop_front();
+                }
+                LOG_BUFFER.emplace_back( tp, file, line, msg, severity );
+                return;
+        }
 
-        std::filesystem::path p{ file };
-
-        std::ostream& os = std::cout;
-
-        auto colorize = [&]( const std::string_view& color, const auto& item ) {
-                os << "\033[38;5;" << color << "m" << item;
-                return "\033[0m";
-        };
-
-        std::time_t t = std::chrono::system_clock::to_time_t( std::chrono::system_clock::now() );
-        os << colorize( tcolor, std::put_time( std::localtime( &t ), "%T" ) ) << " "  // time
-           << colorize( fcolor, p.stem().native() ) << " "                            // filename
-           << colorize( lcolor, line ) << " "                                         // line
-           << msg << "\n";
+        for ( auto [dtp, dfile, dline, dmsg, dsever] : LOG_BUFFER ) {
+                log_print_msg( dtp, dfile, dline, dmsg, dsever );
+        }
+        LOG_BUFFER.clear();
+        log_print_msg( tp, file, line, msg, severity );
 }
 }  // namespace emlabcpp
 
