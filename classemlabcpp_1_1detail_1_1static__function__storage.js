a27deb557eeb0b29b95a38e2bb1d72d31c5f2f67
koniarik <5853442+koniarik@users.noreply.github.com>
2022-08-19 08:49:16 +0000

    Deploying to gh-pages from  @ 6e275bbf63959b2a092e2718162faa7a6f7fb69c 🚀

--- classemlabcpp_1_1detail_1_1static__function__storage.js
+++ classemlabcpp_1_1detail_1_1static__function__storage.js
@@ -4,6 +4,6 @@ var classemlabcpp_1_1detail_1_1static__function__storage =
     [ "static_function_storage", "classemlabcpp_1_1detail_1_1static__function__storage.html#a5159e438627b612591b85ac1dd18a514", null ],
     [ "construct_at", "classemlabcpp_1_1detail_1_1static__function__storage.html#a22de268b5b018e70b55896ef311be3c2", null ],
     [ "handle", "classemlabcpp_1_1detail_1_1static__function__storage.html#a53e9a9605685e24ffd17420b85506baa", null ],
-    [ "invoke", "classemlabcpp_1_1detail_1_1static__function__storage.html#a9cb942aa6f41009555b02217b0cc0aeb", null ],
+    [ "invoke", "classemlabcpp_1_1detail_1_1static__function__storage.html#a5779d64a6885d2f99a366a770debb537", null ],
     [ "vtable", "classemlabcpp_1_1detail_1_1static__function__storage.html#a7c4d484f51daec707f887cff79772f5f", null ]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['init_1091',['init',['../namespaceemlabcpp.html#a1d1440686048e000936426ed21aa3295',1,'emlabcpp']]],
   ['insert_1092',['insert',['../classemlabcpp_1_1contiguous__request__adapter.html#a2b6af1041371a93789cfaf00f1c347ee',1,'emlabcpp::contiguous_request_adapter::insert(node_id parent, const key_type &amp;key, const std::variant&lt; value_type, contiguous_container_type &gt; &amp;val)'],['../classemlabcpp_1_1contiguous__request__adapter.html#a525adea32da75ffa890ec85331f66ee4',1,'emlabcpp::contiguous_request_adapter::insert(node_id parent, const std::variant&lt; value_type, contiguous_container_type &gt; &amp;val)'],['../structemlabcpp_1_1protocol__register__handler.html#aa0553976a34ac75e233343c94fe1fad4',1,'emlabcpp::protocol_register_handler::insert()']]],
-  ['invoke_1093',['invoke',['../classemlabcpp_1_1detail_1_1static__function__storage.html#a9cb942aa6f41009555b02217b0cc0aeb',1,'emlabcpp::detail::static_function_storage']]],
+  ['invoke_1093',['invoke',['../classemlabcpp_1_1detail_1_1static__function__storage.html#a5779d64a6885d2f99a366a770debb537',1,'emlabcpp::detail::static_function_storage']]],
   ['is_5fleft_1094',['is_left',['../classemlabcpp_1_1either.html#a09209aa1fa5a927d0ac4a20846b93639',1,'emlabcpp::either']]]
 ];
