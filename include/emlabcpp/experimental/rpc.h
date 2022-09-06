2ef1a029232294bdc554b24fd717bf4d2ab99107
Jan Veverak Koniarik <squirrelcze@gmail.com>
2022-09-06 10:00:46 +0200

    more improvements to RPC mechanism

--- include/emlabcpp/experimental/rpc.h
+++ include/emlabcpp/experimental/rpc.h
@@ -5,19 +5,61 @@
 namespace emlabcpp::rpc
 {
 
+struct void_return_type
+{
+};
+
+struct reactor_error
+{
+        protocol::error_record record;
+};
+
+struct reply_error
+{
+        std::size_t expected_index;
+        std::size_t index;
+};
+
+struct error
+{
+        std::variant< reactor_error, protocol::error_record, reply_error > er;
+};
+
 template < typename T >
 struct traits;
 
 template < typename... Calls >
 struct traits< std::tuple< Calls... > >
 {
-        using request_type  = std::variant< typename Calls::request... >;
-        using reply_variant = std::variant< typename Calls::reply... >;
-        // TODO: well, the second thing is more like "rpc_reactor_error"
-        using reply_type = std::variant< reply_variant, protocol::error_record >;
+        template < typename Call >
+        struct request_wrapper : protocol::converter_def_type_base
+        {
+                static constexpr auto id = Call::id;
+
+                using def_type = typename Call::request;
+        };
+
+        template < typename Call >
+        struct reply_wrapper : protocol::converter_def_type_base
+        {
+                static constexpr auto id = Call::id;
+
+                static_assert( !std::is_void_v< typename Call::reply > );
+
+                using def_type = typename Call::reply;
+        };
+
+        using request_group = protocol::tag_group< request_wrapper< Calls >... >;
+        using reply_group   = protocol::tag_group< reply_wrapper< Calls >... >;
+
+        using request_type  = typename protocol::traits_for< request_group >::value_type;
+        using reply_variant = typename protocol::traits_for< reply_group >::value_type;
+
+        using outter_reply_group = std::variant< reply_group, reactor_error >;
+        using reply_type         = typename protocol::traits_for< outter_reply_group >::value_type;
 
         using request_traits = protocol::traits_for< request_type >;
-        using reply_traits   = protocol::traits_for< reply_type >;
+        using reply_traits   = protocol::traits_for< outter_reply_group >;
 
         using request_message_type = protocol::message< request_traits::max_size >;
         using reply_message_type   = protocol::message< reply_traits::max_size >;
@@ -30,23 +72,31 @@ public:
         using traits_type          = traits< Def >;
         using reply_variant        = typename traits_type::reply_variant;
         using request_type         = typename traits_type::request_type;
+        using request_group        = typename traits_type::request_group;
         using reply_type           = typename traits_type::reply_type;
+        using outter_reply_group   = typename traits_type::outter_reply_group;
         using request_message_type = typename traits_type::request_message_type;
         using reply_message_type   = typename traits_type::reply_message_type;
 
-        using request_handler = protocol::handler< request_type >;
-        using reply_handler   = protocol::handler< reply_type >;
+        using request_handler = protocol::handler< request_group >;
+        using reply_handler   = protocol::handler< outter_reply_group >;
 
         template < typename Handler >
         static reply_message_type on_message( const request_message_type& msg, Handler&& h )
         {
                 return request_handler::extract( msg )
                     .convert_left( [&]( const request_type& req_var ) {
-                            reply_variant rep = visit( h, req_var );
+                            reply_variant rep = visit_index(
+                                [&]< std::size_t i >() {
+                                        auto val = h( tag< i >{}, *std::get_if< i >( &req_var ) );
+                                        return reply_variant( std::in_place_index< i >, val );
+                                },
+                                req_var );
                             return reply_handler::serialize( rep );
                     } )
                     .convert_right( [&]( const protocol::error_record& rec ) {
-                            return reply_handler::serialize( rec );
+                            EMLABCPP_LOG( "reactor failed to extract incoming message " << msg );
+                            return reply_handler::serialize( reactor_error{ rec } );
                     } )
                     .join();
         }
@@ -67,11 +117,13 @@ public:
         using reply_variant        = typename traits_type::reply_variant;
         using request_type         = typename traits_type::request_type;
         using reply_type           = typename traits_type::reply_type;
+        using request_group        = typename traits_type::request_group;
+        using outter_reply_group   = typename traits_type::outter_reply_group;
         using request_message_type = typename traits_type::request_message_type;
         using reply_message_type   = typename traits_type::reply_message_type;
 
-        using request_handler = protocol::handler< request_type >;
-        using reply_handler   = protocol::handler< reply_type >;
+        using request_handler = protocol::handler< request_group >;
+        using reply_handler   = protocol::handler< outter_reply_group >;
 
         template < auto ID >
         static constexpr std::size_t call_index = get_call_index( ID );
@@ -79,26 +131,36 @@ public:
         template < auto ID >
         using call_type = std::tuple_element_t< call_index< ID >, Def >;
 
-        // TODO: we need better error type /o\.. error_record here can belong to both: reactor and
-        // controller
-        template < auto ID, typename MsgCallback >
-        static either< typename call_type< ID >::reply, protocol::error_record >
-        call( const typename call_type< ID >::request& req, MsgCallback&& cb )
+        template < auto ID, typename... Args, typename MsgCallback >
+        static either< typename call_type< ID >::reply, error >
+        call( MsgCallback&& cb, const Args&... args )
         {
                 using rt = typename call_type< ID >::reply;
 
-                auto req_msg   = request_handler::serialize( req );
+                typename call_type< ID >::request req{ args... };
+                auto                              req_msg = request_handler::serialize(
+                    request_type{ std::in_place_index< call_index< ID > >, req } );
                 auto reply_msg = cb( req_msg );
-                return reply_handler::extract( reply_msg )
-                    .bind_left( [&]( const auto& var ) -> either< rt, protocol::error_record > {
-                            // TODO: extract car properly and decompose errors properly
 
-                            auto* ptr = std::get_if< rt >( std::get_if< 0 >( &var ) );
-                            if ( ptr ) {
-                                    return *ptr;
-                            } else {
-                                    return protocol::error_record{};
+                return reply_handler::extract( reply_msg )
+                    .convert_right( convert_to< error >{} )
+                    .bind_left( [&]( const auto& var ) -> either< rt, error > {
+                            auto* err_ptr = std::get_if< reactor_error >( &var );
+                            if ( err_ptr != nullptr ) {
+                                    return error{ *err_ptr };
                             }
+
+                            auto& reply_var = std::get< 0 >( var );
+                            auto* ptr       = std::get_if< call_index< ID > >( &reply_var );
+                            if ( ptr == nullptr ) {
+                                    EMLABCPP_LOG(
+                                        "controller failed to extract incoming message "
+                                        << reply_msg );
+                                    return error{ reply_error{
+                                        .expected_index = call_index< ID >,
+                                        .index          = reply_var.index() } };
+                            }
+                            return *ptr;
                     } );
         }
 };
@@ -108,12 +170,23 @@ struct derive
 {
         static constexpr auto id = ID;
 
-        static constexpr auto method = Method;
-
         using sig = signature_of< decltype( Method ) >;
 
+        static constexpr bool void_returning = std::is_void_v< typename sig::return_type >;
+
         using request = typename sig::args_type;
-        using reply   = typename sig::return_type;
+        using reply =
+            std::conditional_t< void_returning, void_return_type, typename sig::return_type >;
+
+        static constexpr reply handle( auto& obj, const auto&... args )
+        {
+                if constexpr ( void_returning ) {
+                        ( obj.*Method )( args... );
+                        return void_return_type{};
+                } else {
+                        return ( obj.*Method )( args... );
+                }
+        }
 };
 
 template < typename Class, typename... Bindings >
@@ -134,21 +207,14 @@ public:
                 return reactor_type::on_message( msg, *this );
         }
 
-        template < typename Request >
-        auto operator()( const Request& req )
+        template < std::size_t I, typename Request >
+        auto operator()( tag< I >, const Request& req )
         {
-                static constexpr auto i =
-                    find_if_index< std::tuple_size_v< bindings_tuple > >( []< std::size_t i > {
-                            return std::same_as<
-                                Request,
-                                typename std::tuple_element_t< i, bindings_tuple >::request >;
-                    } );
-                using call_type = std::tuple_element_t< i, bindings_tuple >;
+                using call_type = std::tuple_element_t< I, bindings_tuple >;
 
                 return std::apply(
-                    [&]( const auto&... args ) {
-                            static constexpr auto m = call_type::method;
-                            return ( obj_.*m )( args... );
+                    [&]( const auto&... args ) -> call_type::reply {
+                            return call_type::handle( obj_, args... );
                     },
                     req );
         }
--- tests/rpc_test.cpp
+++ tests/rpc_test.cpp
@@ -1,12 +1,16 @@
 #include "emlabcpp/experimental/rpc.h"
 
+#include "emlabcpp/experimental/pretty_printer.h"
+
 #include <gtest/gtest.h>
 
 using namespace emlabcpp;
 
 enum foo_ids : uint8_t
 {
-        CALL_1
+        CALL_1 = 1,
+        CALL_2 = 2,
+        CALL_3 = 3
 };
 
 struct foo
@@ -15,11 +19,22 @@ struct foo
         {
                 return i * 2;
         }
+
+        std::tuple< int, int > call2_m( float val, int32_t val2 )
+        {
+                return { static_cast< int >( val * static_cast< float >( val2 ) ), 42 };
+        }
+
+        void call3_m( int )
+        {
+        }
 };
 
 using call1 = rpc::derive< CALL_1, &foo::call1_m >;
+using call2 = rpc::derive< CALL_2, &foo::call2_m >;
+using call3 = rpc::derive< CALL_3, &foo::call3_m >;
 
-using test_wrapper = rpc::class_wrapper< foo, call1 >;
+using test_wrapper = rpc::class_wrapper< foo, call1, call2, call3 >;
 
 TEST( rpc, basic )
 {
@@ -27,18 +42,40 @@ TEST( rpc, basic )
 
         test_wrapper wrap{ f };
 
+        // this is callback used to send and receive message between devices, in tests it's just
+        // direct exchange
+        auto exchange_messages_f = [&]( const auto& msg ) {
+                return wrap.on_message( msg );
+        };
+
         using con = rpc::controller< test_wrapper::call_defs >;
 
-        con::call< CALL_1 >(
-            call1::request{ 42 },
-            [&]( const auto& msg ) {
-                    return wrap.on_message( msg );
-            } )
+        con::call< CALL_1 >( exchange_messages_f, 42 )
             .match(
                 [&]( int r ) {
                         EXPECT_EQ( r, 84 );
                 },
                 [&]( auto ) {
                         FAIL() << "got an error";
+                } );
+
+        con::call< CALL_2 >( exchange_messages_f, 0.4f, 666u )
+            .match(
+                [&]( std::tuple< int, int > res ) {
+                        auto [a, b] = res;
+                        EXPECT_EQ( a, 266 );
+                        EXPECT_EQ( b, 42 );
+                },
+                [&]( auto ) {
+                        FAIL() << "got an error";
+                } );
+
+        con::call< CALL_3 >( exchange_messages_f, 42 )
+            .match(
+                [&]( rpc::void_return_type ) {},
+                [&]( rpc::error err ) {
+                        pretty_printer pp;
+                        pp << err;
+                        FAIL() << pp.str();
                 } );
 }
