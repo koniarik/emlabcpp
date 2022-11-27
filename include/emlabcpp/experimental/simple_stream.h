4487be91cb79aeec557a90bb28e129949b99ba57
Jan Veverak Koniarik <squirrelcze@gmail.com>
2022-11-27 12:03:38 +0100

    added support for string_view into simple stream

--- include/emlabcpp/experimental/simple_stream.h
+++ include/emlabcpp/experimental/simple_stream.h
@@ -11,7 +11,7 @@ class simple_stream
 {
 public:
         simple_stream( WriteCallable wc )
-          : writer_( std::move( wc ) )
+          : writer_( std::forward< WriteCallable >( wc ) )
         {
         }
 
@@ -22,56 +22,56 @@ public:
 
         simple_stream& operator<<( char value )
         {
-                writer_( std::string_view{ &value, 1 } );
+                write( std::string_view{ &value, 1 } );
                 return *this;
         }
 
         simple_stream& operator<<( short value )
         {
                 std::snprintf( buffer_.data(), buffer_.size(), "%hi", value );
-                writer_( buffer_.data() );
+                write( buffer_.data() );
                 return *this;
         }
         simple_stream& operator<<( unsigned short value )
         {
                 std::snprintf( buffer_.data(), buffer_.size(), "%hu", value );
-                writer_( buffer_.data() );
+                write( buffer_.data() );
                 return *this;
         }
         simple_stream& operator<<( int value )
         {
                 std::snprintf( buffer_.data(), buffer_.size(), "%i", value );
-                writer_( buffer_.data() );
+                write( buffer_.data() );
                 return *this;
         }
         simple_stream& operator<<( unsigned int value )
         {
                 std::snprintf( buffer_.data(), buffer_.size(), "%u", value );
-                writer_( buffer_.data() );
+                write( buffer_.data() );
                 return *this;
         }
         simple_stream& operator<<( long value )
         {
                 std::snprintf( buffer_.data(), buffer_.size(), "%li", value );
-                writer_( buffer_.data() );
+                write( buffer_.data() );
                 return *this;
         }
         simple_stream& operator<<( unsigned long value )
         {
                 std::snprintf( buffer_.data(), buffer_.size(), "%lu", value );
-                writer_( buffer_.data() );
+                write( buffer_.data() );
                 return *this;
         }
         simple_stream& operator<<( long long value )
         {
                 std::snprintf( buffer_.data(), buffer_.size(), "%lli", value );
-                writer_( buffer_.data() );
+                write( buffer_.data() );
                 return *this;
         }
         simple_stream& operator<<( unsigned long long value )
         {
                 std::snprintf( buffer_.data(), buffer_.size(), "%llu", value );
-                writer_( buffer_.data() );
+                write( buffer_.data() );
                 return *this;
         }
         simple_stream& operator<<( float value )
@@ -79,29 +79,39 @@ public:
                 // TODO: I kinda hate this?
                 std::snprintf(
                     buffer_.data(), buffer_.size(), "%f", static_cast< double >( value ) );
-                writer_( buffer_.data() );
+                write( buffer_.data() );
                 return *this;
         }
         simple_stream& operator<<( double value )
         {
                 std::snprintf( buffer_.data(), buffer_.size(), "%f", value );
-                writer_( buffer_.data() );
+                write( buffer_.data() );
                 return *this;
         }
         simple_stream& operator<<( bool value )
         {
                 std::snprintf( buffer_.data(), buffer_.size(), "%i", value ? 1 : 0 );
-                writer_( buffer_.data() );
+                write( buffer_.data() );
                 return *this;
         }
         simple_stream& operator<<( const void* value )
         {
                 std::snprintf( buffer_.data(), buffer_.size(), "%p", value );
-                writer_( buffer_.data() );
+                write( buffer_.data() );
+                return *this;
+        }
+        simple_stream& operator<<( std::string_view sv )
+        {
+                write( sv );
                 return *this;
         }
 
 private:
+        void write( std::string_view sv )
+        {
+                writer_( sv );
+        }
+
         WriteCallable writer_;
 
         // TODO: is 32 enough?
