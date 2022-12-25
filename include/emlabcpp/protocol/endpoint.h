94215f881edd74aadd6d35c7271520933f2a73d5
Jan Veverak Koniarik <squirrelcze@gmail.com>
2022-12-25 12:19:35 +0100

    cleaned up endpoint a bit

--- include/emlabcpp/protocol/endpoint.h
+++ include/emlabcpp/protocol/endpoint.h
@@ -50,56 +50,7 @@ public:
                     .join();
         }
 
-        template < typename ReadCallback >
-        either< input_value, endpoint_error >
-        load_variant( const std::size_t read_limit, ReadCallback&& read )
-        {
-                // TODO: duplication of sequencer_simple_load
-                // TODO: decompose all of this /o
-                // TODO: error reporting/propagation
-
-                std::size_t                    to_read = sequencer_type::fixed_size;
-                std::optional< input_message > opt_msg;
-                std::size_t                    count = 0;
-                while ( !opt_msg && count < read_limit ) {
-                        std::optional data = read( to_read );
-                        if ( !data ) {
-                                return endpoint_error{ endpoint_load_error{} };
-                        }
-                        seq_.insert( *data );
-                        seq_.get_message().match(
-                            [&to_read, &count]( const sequencer_read_request read_req ) {
-                                    to_read = *read_req;
-                                    count   = 0;
-                            },
-                            [&opt_msg]( const input_message msg ) {
-                                    opt_msg = msg;
-                            } );
-
-                        count += 1;
-                }
-
-                if ( !opt_msg ) {
-                        return endpoint_error{ endpoint_load_error{} };
-                }
-
-                return extract_value( *opt_msg );
-        }
-
 private:
-        either< input_value, endpoint_error > extract_value( const input_message& msg )
-        {
-
-                using handler = packet_handler< InputPacket >;
-
-                return handler::extract( msg ).convert_right(
-                    []( const error_record rec ) -> endpoint_error {
-                            std::ignore = rec;
-                            EMLABCPP_LOG( "Protocol error from endpoint: " << rec );
-                            return endpoint_error{ rec };
-                    } );
-        }
-
         sequencer_type seq_;
 };
 
