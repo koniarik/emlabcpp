452a8cd0cb66f82777c5e51e936a3e3c4ad3ee4d
Jan Veverak Koniarik <squirrelcze@gmail.com>
2023-03-13 20:18:02 +0100

    Added missing requirement

--- scripts/copyright-header-run.sh
+++ scripts/copyright-header-run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# Requires: https://github.com/cloudposse/copyright-header
+
 copyright-header --license MIT --copyright-software emlabcpp --copyright-holder "Jan Veverak Koniarik" --copyright-year "2020" --copyright-software-description "Embedded library" --word-wrap 100 --guess-extension --syntax ./scripts/copyright-header-syntax.yml --add-path src:include:examples:tests:cmake --output-dir ./
