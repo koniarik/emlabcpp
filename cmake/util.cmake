28f006e3b3bc356e3a4b6afc4b441d9ad529e3e2
Jan Veverak Koniarik <squirrelcze@gmail.com>
2024-01-27 10:41:36 +0100

    cmake cleanup

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -20,27 +20,6 @@
 # SOFTWARE.
 #
 
-find_program(
-  EMLABCPP_CLANG_FORMAT_EXECUTABLE
-  NAMES clang-format
-        clang-format-13
-        clang-format-12
-        clang-format-11
-        clang-format-10
-        clang-format-9
-        clang-format-8
-        clang-format-7
-        REQUIRED
-  DOC "clang-format executable")
-
-function(emlabcpp_add_format_test)
-  cmake_parse_arguments(A "" "TARGET;WORKING_DIRECTORY" "FILES" ${ARGN})
-  add_test(
-    NAME ${A_TARGET}
-    COMMAND ${EMLABCPP_CLANG_FORMAT_EXECUTABLE} --dry-run -Werror ${A_FILES}
-    WORKING_DIRECTORY ${A_WORKING_DIRECTORY})
-endfunction()
-
 function(emlabcpp_target_enable_coverage target)
   target_compile_options(${target} PRIVATE --coverage)
   target_link_libraries(${target} gcov)
